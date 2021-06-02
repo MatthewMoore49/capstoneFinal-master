db665a20a45948302c525cdcc73aab5518eb092b
Rachel <rmshinski@gmail.com>
2021-06-02 09:01:53 -0400

    matt

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,19 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-       
-
-
-<h2>Vertical Navigation Bar</h2>
-
-<ul>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
+      <div className="wrapper">
+        <div className="sidebar">
+          <h2>?NAVBAR-PAGE TITLE?</h2>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li><a href="#attractions">Attractions</a></li>
+            </ul>
         </div>
+      </div>
     )
 }
 
