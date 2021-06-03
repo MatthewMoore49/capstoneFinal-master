6bcb69e180d1a8636ac744404e6b81d642f28982
Matt <msm3390@gmail.com>
2021-06-03 09:53:35 -0400

    fingers-crossed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,39 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const path = require('path');
 
+// require('dotenv').config();
+
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import path from 'path';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
+const PORT= process.env.PORT || 5000;
+app.use('/posts', postRoutes);
 
-app.use(bodyParser.json({ limit: "30mb", extended:true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended:true}));
 app.use(cors());
+app.use(express.json());
+app.use(express.static('client/build'));
+const CONNECTION_URL = 'mongodb+srv://general:kableCap21@cluster0.dzt8m.mongodb.net/attractions?retryWrites=true&w=majority'
+
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+//   .then(() => app.listen(PORT, () => console.log('Server is running on port: ${PORT}')))
+//   .catch((error) => console.log(error.message));
+//   mongoose.set('useFindAndModify', false);
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port: ${PORT}');
+});
