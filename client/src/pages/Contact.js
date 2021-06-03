b21f409244195a6556bec118387ffd928a5cc84b
Roosevelt <LarkinsRo@gmail.com>
2021-06-03 13:05:26 -0400

    work

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import Footer from '../components/Footer'   
-import Navbar from '../components/Navbar'
-import '../pages/About.css'
+import axios from 'axios';
+import FooterHome from '../components/FooterHome'
+import '../styles/contact.css'
 
 
 export default class contact extends Component {
-    constructor (props){
-      super(props)
-      this.onChangeName = this.onChangeName.bind(this);
-      this.onChangeEmail = this.onChangeEmail.bind(this);
-      this.onChangeQuestion = this.onChangeQuestion.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
-      this.state = {
-          name: " ",
-          email: " ",
-          comment: " ",
-        }
-      };
+  constructor (props){
+    super(props)
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeQuestion = this.onChangeQuestion.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+        name: " ",
+        email: " ",
+        comment: " ",
+      }
     }
+
+  }
