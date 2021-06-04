3fe5ee1e60a33728932413f8f286ccbb86f67132
Matt <msm3390@gmail.com>
2021-06-04 14:05:16 -0400

    pullit

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,19 @@ const App = () => {
 export default App;
 
 
+const express = require('express');
+const connectDB = require('./config/db');
 
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 
 
