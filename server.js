82509319e168d2eebaa1727a7e511ea171f6a443
Matt <msm3390@gmail.com>
2021-06-04 16:14:27 -0400

    pushing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT= process.env.PORT || 5000;
-app.use('/posts', postRoutes);
+// app.use('/posts', postRoutes);
 
 app.use(cors());
 app.use(express.json());
