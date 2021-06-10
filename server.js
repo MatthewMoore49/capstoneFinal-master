6e0fd9d31f4af99c81f957e35702ae74f9caa181
Matt <msm3390@gmail.com>
2021-06-10 10:10:50 -0400

    push

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+const http = require("http");
 const PORT= process.env.PORT || 5000;
-// app.use('/posts', postRoutes);
+const SERVER = http.createServer();
+SERVER.listen(PORT);
+SERVER.on("listening", () => {
+  console.log("[Server]::LISTEN:%s", PORT);
+});
+SERVER.on("error", error => {
+  throw new Error(`[Server]::ERROR:${error.message}`);
+});
+
 
 app.use(cors());
 app.use(express.json());
