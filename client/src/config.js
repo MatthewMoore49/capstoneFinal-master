ef5fb5eb33765807a7d3ee6c817c50f89a6d26cb
Austin <harrenaustint@gmail.com>
2021-06-10 10:33:19 -0400

    push

--- client/src/config.js
+++ client/src/config.js
@@ -2,4 +2,4 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const BACKEND_URL=process.env.NODE_ENV === 'development'?"http://localhost:5000/": "mongodb+srv://general:kableCap21@cluster0.dzt8m.mongodb.net/attractions?retryWrites=true&w=majority"
+export const BACKEND_URL=process.env.NODE_ENV === 'development'?"http://localhost:5000/": "https://rachdeploy.herokuapp.com/"
