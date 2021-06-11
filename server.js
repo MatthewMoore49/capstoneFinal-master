525ef600d4bbb169cf0d219facb39c4d4d3b3989
Austin <harrenaustint@gmail.com>
2021-06-11 14:09:12 -0400

    pulling

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
-//  const router = require('./routes/Attractions')
 require('dotenv').config();
 
 const app = express();
@@ -17,18 +16,19 @@ const CONNECTION_URL = 'mongodb+srv://general:kableCap21@cluster0.dzt8m.mongodb.
 
 app.use(express.json());
 
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 const attractionsRouter = require('./routes/Attractions');
 
 
 app.use('/attractions', attractionsRouter);
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
