501d30071a25ca54ce1f45713464c4f471e528bf
Rachel <rmshinski@gmail.com>
2021-06-11 17:01:33 -0400

    push to heroku

--- server.js
+++ server.js
@@ -38,7 +38,12 @@ app.listen(PORT, () => {
 
 });
 
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build'))
+  })
+}
 
 
 
