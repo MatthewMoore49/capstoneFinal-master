158033b21dd4fae931885f33b6dfaf0a26e6eb88
Austin <harrenaustint@gmail.com>
2021-06-13 15:55:44 -0400

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
