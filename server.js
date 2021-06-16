444102fa7f76857fef82a6378e1d91029a3dd9ba
Austin <harrenaustint@gmail.com>
2021-06-16 15:26:44 -0400

    push

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/Contact', contactRouter)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
