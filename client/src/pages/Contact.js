c46ff5a72a752a53a3db872c1142e2eaff8d8520
Rachel <rmshinski@gmail.com>
2021-06-16 12:09:38 -0400

    pushing

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -40,7 +40,7 @@ export default class contact extends Component {
       comment: this.state.comment,
     }
     console.log(contact)
-    axios.post(BACKEND_URL + 'contact', contact)
+    axios.post(BACKEND_URL + 'contact/add', contact)
     .then(res=>console.log(res.data));
     // window.location="/"
     this.props.history.push('/');
@@ -50,7 +50,7 @@ export default class contact extends Component {
         email: '',
         comment: ''
     })
-    window.alert('Thank you for your inquiry')
+    window.alert('Thanks for reaching out!')
     console.log(contact)
   }
   render() {
