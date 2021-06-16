3f4b9989f367beee93c8eef9293032abe961b717
Rachel <rmshinski@gmail.com>
2021-06-16 14:38:58 -0400

    push

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT= process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('client/build'));
-const CONNECTION_URL = process.env.BACKEND_URL;
+const CONNECTION_URL = process.env.ATLAS_URI;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
